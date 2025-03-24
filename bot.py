ec44d6f6ebfc114a8ba3be3e82450d5a276d1951
adam12345678909 <adamkadlani@outlook.com>
2025-03-24 18:50:06 +0000

    bot.py
    this is the bot

---
+++ bot.py
@@ -0,0 +1,16 @@
+import pygame
+pygame.init()
+
+user = input("<-you-> ")
+bot = "<-bot-> "
+
+#if
+if user == "hi":
+#print
+      print(bot + " thanks for downloading my GameHub!!! btw let's do something feel free to tell me anything!!!")
+elif user == "your welcome" or user == "ur welcome":
+       print("youre very kind 😊"
+elif user == "who made you?":
+     print("I was created by the brilliant minds of adam! They designed me to be your helpful, engaging, and informative AI companion. My purpose is to assist you with anything from answering questions to brainstorming ideas and solving problems. It's an honor to be a part of your day! 😊")
+
+      
